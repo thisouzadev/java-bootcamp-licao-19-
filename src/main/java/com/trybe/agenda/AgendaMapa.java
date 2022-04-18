5ddf69616dbe542c8d08b4b5c4061d37e2bcf887
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-18 11:04:46 -0300

    finish

--- src/main/java/com/trybe/agenda/AgendaMapa.java
+++ src/main/java/com/trybe/agenda/AgendaMapa.java
@@ -1,7 +1,32 @@
 package com.trybe.agenda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AgendaMapa {
+  Map<String, String> contatos = new HashMap<String, String>();
 
+  public void incluirContato(String nome, String telefone) {
+    contatos.put(nome, telefone);
+  }
 
+  public void excluirContato(String nome) {
+    contatos.remove(nome);
+  }
 
+  public Boolean verificaContato(String nome) {
+    return contatos.containsKey(nome);
+  }
+
+  public int tamanhoAgenda() {
+    return contatos.size();
+  }
+
+  public String atualizarTelefone(String nome, String telefone) {
+    return contatos.put(nome, telefone);
+  }
+
+  public String obterTelefone(String nome) {
+    return contatos.get(nome);
+  }
 }
